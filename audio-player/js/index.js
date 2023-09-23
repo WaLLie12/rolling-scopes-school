ee8f98667a4a43d59d3b194b2d057f2e3f18e8dd
vragno01 <vadimserapionov@gmail.com>
2023-09-23 13:31:55 +0300

    feat: add minor edits

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,126 +1,126 @@
 addEventListener("DOMContentLoaded", () => {
+  const songs = [
+    {
+      name: "Never gonna give you up",
+      artist: "Rick Astley",
+      path: "audio/Never Gonna Give You Up (hardstyle).mp3",
+      cover: "img/Nevergonnagiveyouup.webp",
+    },
+    {
+      name: "RUNNIN THRU THE 7TH WITH MY WOADIES",
+      artist: "$UICIDEBOY$ x POUYA",
+      path: "audio/$UICIDEBOY$ x POUYA - RUNNIN THRU THE 7TH WITH MY WOADIES.mp3",
+      cover: "img/Suicide.png",
+    },
+    {
+      name: "black cotton.the realist killaz.dumpin(izza remix)",
+      artist: "2Pac",
+      path: "audio/2pac black cotton.the realist killaz.dumpin(izzamuzzic remix).mp3",
+      cover: "img/2pac.jpg",
+    },
+    {
+      name: "Air",
+      artist: "Bones",
+      path: "audio/Bones - Air.mp3",
+      cover: "img/bones.jpg",
+    },
+    {
+      name: "Premonition",
+      artist: "Roberto Kan",
+      path: "audio/Roberto Kan - Premonition.mp3",
+      cover: "img/kan.jpg",
+    },
+  ];
 
-const songs = [
-  {
-    name: "Never gonna give you up",
-    artist: "Rick Astley",
-    path: "audio/Never Gonna Give You Up (hardstyle).mp3",
-    cover: "img/Nevergonnagiveyouup.webp",
-  },
-  {
-    name: "RUNNIN THRU THE 7TH WITH MY WOADIES",
-    artist: "$UICIDEBOY$ x POUYA",
-    path: "audio/$UICIDEBOY$ x POUYA - RUNNIN THRU THE 7TH WITH MY WOADIES.mp3",
-    cover: "img/Suicide.png",
-  },
-  {
-    name: "black cotton.the realist killaz.dumpin(izza remix)",
-    artist: "2Pac",
-    path: "audio/2pac black cotton.the realist killaz.dumpin(izzamuzzic remix).mp3",
-    cover: "img/2pac.jpg",
-  },
-  {
-    name: "Air",
-    artist: "Bones",
-    path: "audio/Bones - Air.mp3",
-    cover: "img/bones.jpg",
-  },
-  {
-    name: "Premonition",
-    artist: "Roberto Kan",
-    path: "audio/Roberto Kan - Premonition.mp3",
-    cover: "img/kan.jpg",
-  },
-];
+  const audio = document.querySelector(".audio");
+  const inputSlider = document.querySelector(".input__slider");
+  const inputVolume = document.querySelector(".input__volume");
+  const prevBtn = document.querySelector(".ri-rewind-fill");
+  const nextBtn = document.querySelector(".ri-speed-fill");
+  const pauseBtn = document.querySelector(".ri-pause-circle-fill");
+  const playBtn = document.querySelector(".ri-play-circle-fill");
+  const imgContent = document.querySelector(".album__image");
 
-const audio = document.querySelector(".audio");
-const inputSlider = document.querySelector(".input__slider");
-const inputVolume = document.querySelector(".input__volume");
-const prevBtn = document.querySelector(".ri-rewind-fill");
-const nextBtn = document.querySelector(".ri-speed-fill");
-const pauseBtn = document.querySelector(".ri-pause-circle-fill");
-const playBtn = document.querySelector(".ri-play-circle-fill");
-const imgContainer = document.querySelector("img");
-
-const time = (t) => {
+  const time = (t) => {
     if (isNaN(t)) {
-        return "02:05"; // Handle NaN gracefully
-      }
-      
-  let min = Math.floor(t / 60);
-  if (min < 10) min = "0" + min;
+      return "02:05"; // Handle NaN gracefully
+    }
 
-  let sec = Math.floor(t % 60);
-  if (sec < 10) sec = "0" + sec;
+    let min = Math.floor(t / 60);
+    if (min < 10) min = "0" + min;
 
-  return `${min}:${sec}`;
-};
+    let sec = Math.floor(t % 60);
+    if (sec < 10) sec = "0" + sec;
 
-let musicCounting = 0;
-
-const listMusic = (key) => {
-  const song = songs[key];
-  document.querySelector(".img img").src = song.cover;
-  audio.src = song.path;
-  document.querySelector(".name h1").innerHTML = song.artist;
-  document.querySelector(".name p").innerHTML = song.name;
-  let img = new Image();
-  img.onload = () => {
-    document.body.style.background = `url(${song.cover}) no-repeat center / cover`;
+    return `${min}:${sec}`;
   };
-  img.src = song.cover;
-  
-  setTimeout(() => {
-    inputSlider.max = audio.duration;
-    document.querySelector(".time-two").innerHTML = time(audio.duration);
-  }, 300);
 
-  audio.volume = inputVolume.value / 100;
-};
+  let musicCounting = 0;
 
-const playMusic = () => {
-  audio.play();
-  playBtn.classList.add("hidden");
-  pauseBtn.classList.remove("hidden");
-};
+  const listMusic = (key) => {
+    const song = songs[key];
+    audio.src = song.path;
+    document.querySelector(".name h1").innerHTML = song.artist;
+    document.querySelector(".name p").innerHTML = song.name;
+    let img = new Image();
+    img.onload = () => {
+      imgContent.src = song.cover;
+      document.body.style.background = `url(${song.cover}) no-repeat center / cover`;
+    };
+    img.src = song.cover;
+    setTimeout(() => {
+      inputSlider.max = audio.duration;
+      document.querySelector(".time-two").innerHTML = time(audio.duration);
+    }, 300);
 
-const pauseMusic = () => {
-  audio.pause();
-  pauseBtn.classList.add("hidden");
-  playBtn.classList.remove("hidden");
-};
+    audio.volume = inputVolume.value / 100;
+  };
 
-playBtn.addEventListener("click", playMusic);
-pauseBtn.addEventListener("click", pauseMusic);
+  const playMusic = () => {
+    audio.play();
+    playBtn.classList.add("hidden");
+    pauseBtn.classList.remove("hidden");
+  };
 
-inputVolume.addEventListener("input", () => {
-  audio.volume = inputVolume.value / 100;
-});
+  const pauseMusic = () => {
+    audio.pause();
+    pauseBtn.classList.add("hidden");
+    playBtn.classList.remove("hidden");
+  };
 
-prevBtn.addEventListener("click", () => {
-  musicCounting = (musicCounting - 1 + songs.length) % songs.length;
-  listMusic(musicCounting);
-  playMusic();
-  document.body.classList.add('animation')
-});
+  playBtn.addEventListener("click", playMusic);
+  pauseBtn.addEventListener("click", pauseMusic);
 
-nextBtn.addEventListener("click", () => {
-  musicCounting = (musicCounting + 1) % songs.length;
-  listMusic(musicCounting);
-  playMusic();
-  document.body.classList.add('animation')
-});
+  inputVolume.addEventListener("input", () => {
+    audio.volume = inputVolume.value / 100;
+  });
 
-setInterval(() => {
+  prevBtn.addEventListener("click", () => {
+    musicCounting = (musicCounting - 1 + songs.length) % songs.length;
+    listMusic(musicCounting);
+    playMusic();
+    document.body.classList.add("animation");
+    imgContent.classList.add("animation");
+  });
+
+  nextBtn.addEventListener("click", () => {
+    musicCounting = (musicCounting + 1) % songs.length;
+    listMusic(musicCounting);
+    playMusic();
+    document.body.classList.add("animation");
+    imgContent.classList.add("animation");
+  });
+
+  setInterval(() => {
     if (!isNaN(audio.duration)) {
-        inputSlider.max = audio.duration;
-        document.querySelector('.time-two').innerHTML = time(audio.duration);
+      inputSlider.max = audio.duration;
+      document.querySelector(".time-two").innerHTML = time(audio.duration);
     }
     if (!isNaN(audio.currentTime)) {
-    inputSlider.value = audio.currentTime;
-    document.querySelector('.time-one').innerHTML = time(audio.currentTime);
+      inputSlider.value = audio.currentTime;
+      document.querySelector(".time-one").innerHTML = time(audio.currentTime);
     }
-  
+
     if (audio.currentTime >= inputSlider.max) {
       if (musicCounting >= songs.length - 1) musicCounting = 0;
       else musicCounting++;
@@ -128,12 +128,10 @@ setInterval(() => {
       playMusic();
     }
   }, 500);
-  
 
-inputSlider.addEventListener(('change'),()=>{
-    audio.currentTime = inputSlider.value
-})
+  inputSlider.addEventListener("change", () => {
+    audio.currentTime = inputSlider.value;
+  });
 
-listMusic(0)
-
+  listMusic(0);
 });
